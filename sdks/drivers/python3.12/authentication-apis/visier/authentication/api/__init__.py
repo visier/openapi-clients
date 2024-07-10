9ac700447957e3a8f0b2ebc5d497008aaba40eb3
timur-malikov_visier <timur.malikov@visier.com>
2024-07-09 18:13:23 -0700

    [VAN-122986] Changed folder structure

--- sdks/drivers/python3.12/authentication-apis/visier/authentication/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from visier.authentication.api.authentication_api import AuthenticationApi
-
--- sdks/drivers/python3.12/model-query-apis/visier/model_query/api/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from visier.model_query.api.data_model_api import DataModelApi
-from visier.model_query.api.query_api import QueryApi
-
