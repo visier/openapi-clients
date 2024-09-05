d90b5bdfd15591c6eea7ce921267f67d4b3983ec
timur-malikov_GIDT <timur.malikov@visier.com>
2024-09-04 18:17:06 -0700

    [VAN-1257990] Updated workflow to use preprocessed specifications.

--- sdk-blueprints/python/generate-all.sh
+++ sdk-blueprints/python/generate-all.sh
@@ -3,20 +3,21 @@
 set -e
 
 output_dir=$1
+spec_dir=$2
 
-# Check if the required argument is provided
-if [ -z "$output_dir" ]; then
-  echo "Error: Missing required argument."
-  echo "Usage: $0 <output_dir>"
-  echo "Provided argument: output_dir=${output_dir:-<not provided>}"
+# Check if the required arguments are provided
+if [ -z "$output_dir" ] || [ -z "$spec_dir" ]; then
+  echo "Error: Missing required arguments."
+  echo "Usage: $0 <output_dir> <spec_dir>"
+  echo "Provided arguments: output_dir=${output_dir:-<not provided>}, spec_dir=${spec_dir:-<not provided>}"
   exit 1
 fi
 
 # Get the directory of the current script
 script_dir=$(dirname "$0")
 
-"$script_dir"/generate-package.sh visier.sdk.api.core res/authentication-apis.yaml sdk-blueprints/python/core "$output_dir"
-"$script_dir"/generate-package.sh visier.sdk.api.analytic_model res/analytic-model-apis.yaml sdk-blueprints/python/api "$output_dir"
-"$script_dir"/generate-package.sh visier.sdk.api.administration res/administration-apis.yaml sdk-blueprints/python/api "$output_dir"
-"$script_dir"/generate-package.sh visier.sdk.api.data_in res/data-in-apis.yaml sdk-blueprints/python/api "$output_dir"
-"$script_dir"/generate-package.sh visier.sdk.api.data_out res/data-out-apis.yaml sdk-blueprints/python/api "$output_dir"
+"$script_dir"/generate-package.sh visier.sdk.api.core "$spec_dir/authentication-apis.yaml" sdk-blueprints/python/core "$output_dir"
+"$script_dir"/generate-package.sh visier.sdk.api.analytic_model "$spec_dir/analytic-model-apis.yaml" sdk-blueprints/python/api "$output_dir"
+"$script_dir"/generate-package.sh visier.sdk.api.administration "$spec_dir/administration-apis.yaml" sdk-blueprints/python/api "$output_dir"
+"$script_dir"/generate-package.sh visier.sdk.api.data_in "$spec_dir/data-in-apis.yaml" sdk-blueprints/python/api "$output_dir"
+"$script_dir"/generate-package.sh visier.sdk.api.data_out "$spec_dir/data-out-apis.yaml" sdk-blueprints/python/api "$output_dir"
