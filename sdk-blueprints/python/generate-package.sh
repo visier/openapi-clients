ceb243bfeaabc7a1c1a77fa6890c53f6db55a25a
timur-malikov_GIDT <timur.malikov@visier.com>
2024-09-04 17:53:19 -0700

    0.0.200

--- sdk-blueprints/python/generate-package.sh
+++ sdk-blueprints/python/generate-package.sh
@@ -20,7 +20,7 @@ output_api_dir="$4/$package_name"
 templates_dir="$blueprints_dir/templates"
 
 # Extract the specification version
-spec_version=$(awk '/version:/ {gsub(/"| /, "", $2); print $2}' "$spec_file")
+spec_version="0.0.200"
 
 echo "Generating Visier API $package_name $spec_version $spec_file"
 
