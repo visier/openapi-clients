6744f7a61067dabaf4c09d922936922d444d3f22
timur.malikov <timur.malikov@visier.com>
2024-10-08 15:14:48 -0700

    [VAN-126702] Added version and projects_dir as parameters.

--- sdk-blueprints/update-schema.sh
+++ sdk-blueprints/update-schema.sh
@@ -1,16 +1,20 @@
 # Internal script for debugging SDK generation locally.
 # Run from the root of the openapi-clients repository.
 
+# Exit immediately if any command exits with a non-zero status
+set -e
+
+# Define the version of the openapi specification
+version="${1:-0.0.001}"
+
 # Define directory where projects are stored
-projects_dir="$HOME/projects"
+projects_dir="${2:-$HOME/projects}"
 
 vserver_dir="$projects_dir/com.visier.vserver"
 python_sdk_repository_dir="$projects_dir/python-sdk/src"
 
-version="22222222.99201.1494"
-
 # Before running this script docker image should be build from repository com.visier.containers.openapi
-docker container run -u $UID --rm -v "$vserver_dir":/vserver -e VERSION=$version -e OPENAPI_OUTPUT_ROOT="/vserver/target/openapi" -e GENERATE_API_DESCRIPTIONS=true visier-openapi:latest
+docker container run -u $UID --rm -v "$vserver_dir":/vserver -e VERSION=$version -e OPENAPI_OUTPUT_ROOT="/vserver/target/openapi" -e GENERATE_API_DESCRIPTIONS=false visier-openapi:latest
 
 # Copy openapi specifications to res directory
 cp "$vserver_dir/target/openapi"/*.yaml res/
