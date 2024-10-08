faa4e27ad701e5ecf7e9b67b66811164a39b03fc
timur.malikov <timur.malikov@visier.com>
2024-10-08 11:04:52 -0700

    [VAN-126702] Updated oneof mustache file not to use empty DTOs as a result. Created script for end-to-end generation SDKs.

---
+++ sdk-blueprints/update-schema.sh
@@ -0,0 +1,22 @@
+# This script is for internal usage to debug SDK generation locally
+# This script should be run from the root of the openapi-clients repository (current repository)
+
+# Define directory where projects are stored
+projects_dir="$HOME/projects"
+
+vserver_dir="$projects_dir/com.visier.vserver"
+python_sdk_repository_dir="$projects_dir/python-sdk/src"
+
+version="22222222.99201.1494"
+
+# Before running this script docker image should be build from repository com.visier.containers.openapi
+docker container run -u $UID --rm -v "$vserver_dir":/vserver -e VERSION=$version -e OPENAPI_OUTPUT_ROOT="/vserver/target/openapi" -e GENERATE_API_DESCRIPTIONS=true visier-openapi:latest
+
+# Copy openapi specifications to res directory
+cp "$vserver_dir/target/openapi"/*.yaml res/
+
+# Run preprocessing openapi specs script
+python3 "sdk-blueprints/preprocessing/preprocessing.py" res "sdk-generation-test/specs"
+
+# Run python SDK generation script
+./sdk-blueprints/python/generate-all.sh "$python_sdk_repository_dir" "sdk-generation-test/specs"
